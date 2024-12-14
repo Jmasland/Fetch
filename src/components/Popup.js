6d1cb010612b50431a2943aa9b8dd889292876c1
Jmasland <116884669+Jmasland@users.noreply.github.com>
2024-12-14 18:19:12 +0530

    Update Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -8,9 +8,9 @@ import { FaUser, FaPhone, FaEnvelope } from "react-icons/fa6";
 import FetchLogo from "../assets/Fetch-logo.png";
 import { SubmitIcon } from "../svgIcon/svg";
 
-const SERVICE_ID = "service_vba6q7k";
-const TEMPLATE_ID = "template_lhmteko";
-const USER_ID = "POtCyAWG-0jIp4g8S";
+const SERVICE_ID = "service_81sk69p";
+const TEMPLATE_ID = "template_jrirbr9";
+const USER_ID = "YZ7pUGvoXY7A6lV5S";
 
 
 const Popup = props => {
