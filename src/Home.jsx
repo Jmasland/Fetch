5992ea583fb3a2b95cd484587a3ebf5939153f34
Jmasland <116884669+Jmasland@users.noreply.github.com>
2025-01-07 18:21:22 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "./viewport/header";
+import FavIcon from "./assets/favicon.png";
 import Footer from "./viewport/footer";
 import Main from "./viewport/main";
 
 function App() {
   return (
     <div className="wrapper">
+      <Favicon url={FavIcon}></Favicon>
       <Header />
 
       <Main />
